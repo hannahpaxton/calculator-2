7322d077d7ceee9f7bc63167a6baf6c0c8bf6342
Hannah Paxton <hannahlinnpaxton@gmail.com>
2021-03-17 21:42:03 -0400

    Establish while loop and initial operations

--- calculator.py
+++ calculator.py
@@ -1,7 +1,37 @@
 """CLI application for a prefix-notation calculator."""
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
+                        power, mod, add_mult, add_cubes)
 
 
 # Replace this with your code
+
+
+#get the user input
+#if input == q, exit loop 
+#tokenize the input
+#check if operand equals a certain value
+#pass the numbers through the equation that the operand suggests
+#assign the eqaution to a variable and print the variable
+
+#accept user input
+
+
+while True:
+    user_expression = input("Enter your equation!")
+    if user_expression != "q":
+        tokenized_input = user_expression.split(' ')
+        num1 = int(tokenized_input[1])
+        num2 = int(tokenized_input[2])
+
+        if tokenized_input[0] == "+":
+            result = add(num1,num2)
+
+        elif tokenized_input[0] == "-":
+            result = subtract(num1,num2)
+        
+    elif user_expression =="q":
+        break
+            
+    print (result)
+
